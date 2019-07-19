e06a2ea786e3db2d04766d79dc16e97499af101e
Gennadiy <genaydzhan70@gmail.com>
2019-07-19 15:53:09 +0600

    Control jump angle

--- src/ball.js
+++ src/ball.js
@@ -1,7 +1,45 @@
-export const Ball = function(x, y) {
-  function xOfTime(time) {
-    
+export const Ball = (function(x, y) {
+
+  function getXofTime(x0, angle) {
+    const v = 1500; // px/second
+    const cosAngle = Math.cos(angle);
+    return function(time) {
+      const t = msToSeconds(time);
+      return x0 + v * cosAngle * t; // px
+    }
   }
 
-  return {x, y, xOfTime};
-}
+  function getYofTime(x0, angle) {
+    const v = 1500; // px/second
+    const a = -3000; // px/second^2
+    const sinAngle = Math.sin(angle);
+    return function(time) {
+      const t = msToSeconds(time);
+      return x0 + v * sinAngle * t + a * t**2 / 2; // px
+    }
+  }
+
+  function xOfTime(time) {
+    const v = 2000; // px/second
+    const a = 0; // px/second^2
+    const t = msToSeconds(time);
+    return v * t + a * t * t / 2;; // px
+  }
+
+  function yOfTime(time) {
+    const v = 500; // px/second
+    const a = -1000; // px/second^2
+    const t = msToSeconds(time);
+    return v * t + a * t * t / 2;
+  }
+
+  function yVelocity(time) {
+
+  }
+
+  function msToSeconds(ms) {
+    return ms / 1000;
+  }
+
+  return {x, y, getXofTime, getYofTime};
+})();
--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 "use strict";
-import Ball from "./ball.js";
+import {Ball} from "./ball.js";
 
-function DOMStuff() {
+function GameGUI() {
   const container = document.querySelector(".content");
-  const width = container.offsetWidth;
+  const containerWidth = container.offsetWidth;
+  const containerHeight = container.offsetHeight;
 
   const ball = document.createElement("div");
   ball.classList.add("ball");
-  ball.style.width = 70 + "px";
-  ball.style.height = 70 + "px";
+  ball.style.width = 50 + "px";
+  ball.style.height = 50 + "px";
   ball.style.left = 0;
   ball.style.bottom = 0;
   container.appendChild(ball);
 
-  ball.addEventListener("click", function() {
+  container.addEventListener("click", jump);
+
+  function jump(e) {
+    console.log(angle(e));
+    const x = Ball.getXofTime(parseInt(ball.style.left), angle(e));
+    const y = Ball.getYofTime(parseInt(ball.style.bottom), angle(e));
     let start = performance.now();
     requestAnimationFrame(function animate() {
-      //console.log(parseInt(ball.style.left), parseInt(ball.style.bottom));
       let time = performance.now() - start;
-      console.log(time);
+      // If ball is not underground
       if (parseInt(ball.style.bottom) >= 0) {
         ball.style.left = x(time) + "px";
         ball.style.bottom = y(time) + "px";
@@ -28,25 +33,17 @@ function DOMStuff() {
         ball.style.bottom = 0;
       }
     })
-  })
-
-  function x(time) {
-    const v = 500;
-    const a = 0;
-    const t = msToSeconds(time);
-    return v * t + a * t * t / 2;;
   }
 
-  function y(time) {
-    const v = 500;
-    const a = -1000;
-    const t = msToSeconds(time);
-    return v * t + a * t * t / 2;
+  function angle(e) {
+    let x = e.pageX - parseInt(ball.style.left);
+    let y = (containerHeight - e.pageY) - parseInt(ball.style.bottom);
+    return arcctg(x / y);
   }
 
-  function msToSeconds(ms) {
-    return ms / 1000;
+  function arcctg(x) {
+    return Math.PI / 2 - Math.atan(x);
   }
 };
 
-DOMStuff();
+GameGUI();
