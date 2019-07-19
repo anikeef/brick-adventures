d0372e6dd03c62cefdd37106ea194e0e0adc0373
Gennadiy <genaydzhan70@gmail.com>
2019-07-19 13:43:30 +0600

    Use strict and create webpack config

--- main.js
+++ main.js
@@ -1,1 +1,113 @@
-!function(t){var e={};function n(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:r})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)n.d(r,o,function(e){return t[e]}.bind(null,o));return r},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=0)}([function(e,n){!function(){const e=document.querySelector(".content"),n=e.offsetWidth/20,r=document.createElement("div");function o(e){return v=15,a=0,t=i(e),v*t+a*t*t/2}function u(t){return t*n+"px"}function i(t){return t/1e3}r.classList.add("ball"),r.style.width=u(1),r.style.height=u(1),r.style.left=0,r.style.bottom=0,e.appendChild(r),r.addEventListener("click",function(){start=performance.now(),requestAnimationFrame(function e(){console.log(parseInt(r.style.left),parseInt(r.style.bottom)),time=performance.now()-start,parseInt(r.style.bottom)>=0?(r.style.left=u(o(time)),r.style.bottom=u(function(e){return v=20,a=-40,t=i(e),v*t+a*t*t/2}(time)),requestAnimationFrame(e)):(r.style.left=u(o(time)),r.style.bottom=0)})})}()}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/ball.js":
+/*!*********************!*\
+  !*** ./src/ball.js ***!
+  \*********************/
+/*! exports provided: Ball */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"Ball\", function() { return Ball; });\nconst Ball = function(x, y) {\n  function xOfTime(time) {\n\n  }\n\n  return {x, y, xOfTime};\n}\n\n\n//# sourceURL=webpack:///./src/ball.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ball_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ball.js */ \"./src/ball.js\");\n\n\n\nfunction DOMStuff() {\n  const container = document.querySelector(\".content\");\n  const width = container.offsetWidth;\n  const pxPerMeter = width / 20;\n\n  const ball = document.createElement(\"div\");\n  ball.classList.add(\"ball\");\n  ball.style.width = metersToPx(1);\n  ball.style.height = metersToPx(1);\n  ball.style.left = 0;\n  ball.style.bottom = 0;\n  container.appendChild(ball);\n\n  ball.addEventListener(\"click\", function() {\n    let start = performance.now();\n    requestAnimationFrame(function animate() {\n      console.log(parseInt(ball.style.left), parseInt(ball.style.bottom));\n      let time = performance.now() - start;\n      if (parseInt(ball.style.bottom) >= 0) {\n        ball.style.left = metersToPx(x(time));\n        ball.style.bottom = metersToPx(y(time));\n        requestAnimationFrame(animate);\n      } else {\n        ball.style.left = metersToPx(x(time));\n        ball.style.bottom = 0;\n      }\n    })\n  })\n\n  function x(time) {\n    const v = 15;\n    const a = 0;\n    const t = msToSeconds(time);\n    return v * t + a * t * t / 2;;\n  }\n\n  function y(time) {\n    const v = 20;\n    const a = -40;\n    const t = msToSeconds(time);\n    return v * t + a * t * t / 2;\n  }\n\n  function metersToPx(meters) {\n    return meters * pxPerMeter + \"px\";\n  }\n\n  function msToSeconds(ms) {\n    return ms / 1000;\n  }\n};\n\nDOMStuff();\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ });
