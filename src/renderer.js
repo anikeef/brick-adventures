c090e81a5d7737bc0ddc2869a2fdc802cfca8c89
Gennadiy <genaydzhan70@gmail.com>
2019-09-01 21:51:51 +0600

    Use canvas instead of DOM

--- src/renderer.js
+++ src/renderer.js
@@ -4,8 +4,12 @@ import { map, filter } from 'rxjs/operators';
 export function Renderer(game) {
   const container = document.querySelector('.gamebox');
   const containerHeight = container.offsetHeight;
-  const brick = initializeElement(game.brick, 'brick');
-  const blocks = game.blocks.map((model) => initializeElement(model, 'block'));
+  const canvas = document.querySelector('canvas');
+  canvas.width = container.offsetWidth;
+  canvas.height = container.offsetHeight;
+  const ctx = canvas.getContext('2d');
+  ctx.transform(1, 0, 0, -1, 0, canvas.height);
+
   const jumpTrigger$ = fromEvent(container, 'click')
     .pipe(
       filter(() => game.canJump),
@@ -21,20 +25,15 @@ export function Renderer(game) {
     game.jump(event);
   })
 
-  function initializeElement(model, type) {
-    let element = document.createElement('div');
-    element.classList.add(type);
-    element.style.width = model.width + 'px';
-    element.style.height = model.height + 'px';
-    element.style.bottom = model.y + 'px';
-    element.style.left = model.x + 'px';
-    container.appendChild(element);
-    return element;
-  }
-
   function render() {
-    brick.style.bottom = game.brick.y + 'px';
-    brick.style.left = game.brick.x + 'px';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(game.brick.x, game.brick.y, game.brick.width, game.brick.height);
+
+    ctx.fillStyle = '#D2D2D2';
+    game.blocks.forEach((block) => {
+      ctx.fillRect(block.x, block.y, block.width, block.height);
+    })
   }
 
   return { render };
